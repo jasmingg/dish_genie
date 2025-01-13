450dd917445afa1dc317b5ccf1bb256fa2b217c6
jasmingg <jasmingiri2006@gmail.com>
2025-01-12 22:36:49 -0500

    #1 Fixing bug for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [
-		react()
-	]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu'],
+    },
+  },
 })
